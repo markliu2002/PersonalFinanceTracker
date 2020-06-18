e725968a32fc23a7694b0dec9fb163d4a24f6af3
Mark Liu <markliu202@gmail.com>
2020-06-18 14:22:01 -0400

    Updates

---
+++ precache-manifest.0750ba27123b5c7649686af992a8b888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5365553a9d18866566fe1b59cc9821fe",
+    "url": "/PersonalFinanceTracker/index.html"
+  },
+  {
+    "revision": "0145c4484e1641931b86",
+    "url": "/PersonalFinanceTracker/static/css/main.f10b82ea.chunk.css"
+  },
+  {
+    "revision": "38b2244c650eacc1c65e",
+    "url": "/PersonalFinanceTracker/static/js/2.4baf080b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PersonalFinanceTracker/static/js/2.4baf080b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0145c4484e1641931b86",
+    "url": "/PersonalFinanceTracker/static/js/main.995983dc.chunk.js"
+  },
+  {
+    "revision": "5e596c70fa9e5f80f41e",
+    "url": "/PersonalFinanceTracker/static/js/runtime-main.26fceb1b.js"
+  }
+]);
